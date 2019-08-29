6aa919f89e16f20d50ae38604057f495e1c1a1b6
mikemyzhao <tjzhaomengyi@163.com>
2019-08-29 18:43:38 +0800

    【初始化】工程pom文件

---
+++ src/main/java/com/lanqiao/entity/Student.java
@@ -0,0 +1,45 @@
+package com.lanqiao.entity;
+
+/**
+ * @Auther: zhaomengyi
+ * @Date: 2019/8/29 18:29
+ * @Description:
+ */
+public class Student {
+    private int stuNo;
+    private String stuName;
+    private int stuAge;
+
+    public int getStuNo() {
+        return stuNo;
+    }
+
+    public void setStuNo(int stuNo) {
+        this.stuNo = stuNo;
+    }
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    public int getStuAge() {
+        return stuAge;
+    }
+
+    public void setStuAge(int stuAge) {
+        this.stuAge = stuAge;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "stuNo=" + stuNo +
+                ", stuName='" + stuName + '\'' +
+                ", stuAge=" + stuAge +
+                '}';
+    }
+}
